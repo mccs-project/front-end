c8f6fe9aeb27f2ae09e7ca1e4b41a63ccd2ee6c1
Nii <nii.oncasi@gmail.com>
2022-07-12 14:18:04 +0900

    git cloneの形式に変更

---
+++ tools/link-shared.js
@@ -0,0 +1,55 @@
+//  srcフォルダ内にsharedのシンボリックリンクを貼るスクリプト
+//  
+//  【前提条件】
+//  ・以下のフォルダ構成になっていること
+//    mccs-project
+//    ├─common-shared
+//    └─[本プロジェクト]
+//      └─src
+//  ・プロジェクトのルートで実行すること
+
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require('path');
+
+const projectRoot = process.cwd();
+
+//  shared作成後のパスが存在する場合は処理抜け（何もしない）
+const sharedDirPath = path.join(projectRoot, "src", "shared");
+if (fs.existsSync(sharedDirPath)) {
+    return;
+}
+//  存在しない場合はgit clone
+else {
+    exec("git clone https://github.com/mccs-project/common-shared.git ./src/shared", (err, stdout, stderr) => {
+        if(err) { throw err; }
+        console.log(stdout);
+    });
+}
+
+// //  このプロジェクトのフォルダと同じ階層にcommon-sharedが存在しない場合は異常終了させる
+// const sharedRepoPath = path.join(projectRoot, "/../common-shared");
+// if(fs.existsSync(sharedRepoPath) == false) {
+//     console.error(`『${sharedRepoPath}』 is not exists.`);
+//     throw new Error("{B0071D43-9F12-4B13-94AF-163F990DCC0C}");
+// }
+
+// //  シンボリックリンクの作成
+// //  Windowsの場合、出力が文字化けするが、ライブラリを別途インストールしたくないため無視
+// exec("npm link ../common-shared", (err, stdout, stderr) => {
+//     if(err) { throw err; }
+//     console.log(stdout);
+
+//     //  シンボリックリンクを移動
+//     const mvCmd = process.platform === "win32" ? "move" : "mv";
+//     exec(`${mvCmd} ${path.join(projectRoot, "node_modules", "mccs-common-shared")} ${path.join(projectRoot, "src", "shared")}`, (err, stdout, stderrr) =>{
+//         if(err) { throw err; }
+//         console.log(stdout);
+
+//         //  npm iでnode_modules内のpackage-lock.jsonに記載されているlinkを除去
+//         exec("npm i", (err, stdout, stderr)=>{
+//             if(err) { throw err; }
+//             console.log(stdout);
+//         });
+//     });
+// });
